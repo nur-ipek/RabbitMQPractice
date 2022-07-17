3873f798db088c293437dfaca1017821f9173ec6
NurİpekTiryaki <ipek.tiryaki@extraloyalty.com>
2022-07-17 19:10:27 +0300

    Add project files.

---
+++ RabbitMQPractice.publisher/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using RabbitMQPractice.publisher;
+
+Console.WriteLine("Hello, World!");
+
+var pub = new Publisher();
+pub.pub();
+
---
+++ RabbitMQPractice.publisher/Publisher.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQPractice.publisher
+{
+    public class Publisher
+    {
+       public void pub()
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bbbsxkbx:zMwgNNn1ygxD4ycevJcrnCgLiEGD9XBw@rat.rmq2.cloudamqp.com/bbbsxkbx");
+
+            using var conneciton = factory.CreateConnection();
+
+            var channel = conneciton.CreateModel();
+
+            var queue = channel.QueueDeclare("test-queue", true, false, false);
+
+            var message = "Hello World!";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty,"test-queue",null,messageBody);
+
+            Console.WriteLine("Mesaj kuyruğa eklendi");
+
+            Console.ReadLine();
+        }
+    }
+}
